0b8af7c36f615aee62474b808db626263c2846cd
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-03-09 17:24:29 +0530

    hii

---
+++ PRACTISE QUESTION/average.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float phy=35;
+    float chem=88;
+    float math=92;
+    float MP=55;
+    float bio=67;
+    float average=(phy+chem+math+MP+bio)/5;
+    cout<<average;
+    return 0;
+}
