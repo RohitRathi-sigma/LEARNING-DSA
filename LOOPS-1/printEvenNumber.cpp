38430c47d5d08f842b5a14c7e5804e6fde495250
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-03-11 22:34:45 +0530

    Even num from 1 to 100

---
+++ LOOPS-1/printEvenNumber.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+
+cout<<"Even numbers from 1 to 100:"<<endl;
+for(int i=1;i<101;i++)
+{
+    if(i%2==0)
+    {
+        cout<<i<<"is even number"<<endl;
+    }
+}
+    return 0;
+}
