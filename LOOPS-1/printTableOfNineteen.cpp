aed69b344d530898f3e72afd8021b69281d04df2
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-03-11 23:19:42 +0530

    Table of nineteen

---
+++ LOOPS-1/printTableOfNineteen.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=19;
+    cout << "The table of nineteen :"<<endl;
+    for (int i = 1; i <11; i++)
+    {
+        cout << i * n << endl;
+    }
+    return 0;
+}
