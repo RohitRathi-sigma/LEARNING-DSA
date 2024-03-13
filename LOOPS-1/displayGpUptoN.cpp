cccad8e22d39ff2434e08f40487335bb31224644
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-03-13 22:05:55 +0530

    Display the GP as 3,12,48

---
+++ LOOPS-1/displayGpUptoN.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of terms :";
+    cin >> n;
+    int a=3;
+    for(int i=1;i<=n;i++){
+        cout<<a<<endl;
+        a*=4;
+    }
+    return 0;
+}
