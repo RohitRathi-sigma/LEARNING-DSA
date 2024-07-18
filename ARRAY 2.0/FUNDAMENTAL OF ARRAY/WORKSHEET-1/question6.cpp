16abb0e73212401c22fb444fe06f239218fe4e48
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-07-18 17:24:08 +0530

    third element

---
+++ ARRAY 2.0/FUNDAMENTAL OF ARRAY/WORKSHEET-1/question6.cpp
@@ -0,0 +1,13 @@
+//  Given an array int arr[5] = {10, 20, 30, 40, 50};, write the code to access and print the third element.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    cout << "The third element of array is : " << arr[2];
+
+    return 0;
+}
