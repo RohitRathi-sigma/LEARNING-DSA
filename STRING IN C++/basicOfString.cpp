602506a2198647fd72e70f65b3855f224d83e696
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-07-29 23:14:52 +0530

    how to print an string

---
+++ STRING IN C++/basicOfString.cpp
@@ -0,0 +1,11 @@
+//how to print an string.
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    char str[5]={'a','b','c','d','e'};
+    for(int i=0;i<5;i++){
+        cout<<str[i]<<endl;
+    }
+    return 0;
+}
