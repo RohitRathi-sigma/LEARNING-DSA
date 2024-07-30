f9164c4d2c30e61b1393fea22fd64b99e0b7ba3e
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-07-30 22:31:39 +0530

    count consonants in string

---
+++ STRING IN C++/WORKSHEET 1/countAllConsonants.cpp
@@ -0,0 +1,20 @@
+// Input a string of length n and count all the consonants in the given string’
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    cout << "Enter the string :";
+    getline(cin, s);
+    int count = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+       char c = tolower(s[i]);     //To count both small & capital letters
+        if ((c >= 'a' && c <= 'z') && !(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')) {
+            count++;
+        }
+    }
+    cout << "Number of consonants in string are : " << count << endl;
+    return 0;
+}
